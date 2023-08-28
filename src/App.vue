760f7deb5439fc6a5e2be1f4312ad1d91d23f741
Karolina del Pilar Gonzáles Caramantín <karolina.gonzales@entelgy.pe.local>
2023-08-28 18:32:19 -0500

    unit 3

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import RobotBuilder from './home/build/RobotBuilder.vue'
+export default {
+  name: 'App',
+  components: {
+    RobotBuilder
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <main>
+    <RobotBuilder></RobotBuilder>
+  </main>
 </template>
 
 <style scoped>
